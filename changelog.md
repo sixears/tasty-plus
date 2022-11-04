2895453eceb4665f0127796d75dffdbfd404f18c
Mr. Fluffy <git@sixearsm>
2022-11-04 14:49:25 +0000

    fix package names

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+1.5.2.9 2022-11-04
+==================
+- fix package names
+
 1.5.2.8 2022-11-03
 ==================
 - remove redundant "output" flake-utils
--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.6";
 
-    exited.url        = "github:sixears/exited/r1.0.4.9";
+    exited.url        = "github:sixears/exited/r1.0.4.10";
     more-unicode.url  = "github:sixears/more-unicode/r0.0.17.6";
   };
 
